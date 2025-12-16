8e4e423ded4dd72fddee3987b1f5b02da48943f8
yug <yugchapherkar2@gmail.com>
2025-12-17 01:45:21 +0530

    sample readme.md and code base of day 1 and day 2 and day 2

---
+++ javabasics.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class javabasics {
+    public static void main (String[] args) {
+    //    int a=5;
+    //    int b=10;
+    //    int sum=a+b;
+    //    String name="Hello World";
+    //    System.out.println(name);
+    //    System.out.println(a);
+    //    System.out.println("The sum of a and b is :" +sum);  
+    Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter 1st number");
+    // int a = sc.nextInt();
+    // System.out.println("Enter 2nd number");
+    // int b = sc.nextInt();
+    // int sum = a + b;
+    // System.out.println("The sum of a and b is :" + sum);
+System.out.println("Enter the radius of circle");
+    float rad = sc.nextFloat();
+float area = 3.14f * rad * rad;
+System.out.println("The area of circle is :" + area);
+}
+}
---
+++ javad2.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class javad2 {
+    public static void main (String[] args){
+        int a=5;
+        int b=10;
+        //System.out.println("The value of a is :" + (a+b));
+
+        //Unary operator [DAY 3]
+        //a=a+1; === a++;
+        // pre increment ++a === value is increased first and then used in expression
+        // post increment a++ === value is used first in expression and then increased
+
+
+        // System.out.println("The value of a is :" + (a--));
+        // System.out.println("The value of a is :" + (a));
+
+        // System.out.println("The value of a is :" + (a++));
+        // System.out.println("The value of a is :" + (a));
+
+        // Relational Operators
+        // System.out.println(a==b);  
+        // System.out.println(a!=b);
+        // System.out.println(a>=b);
+        // System.out.println(a<=b);
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+
+       // Logical Operators
+        // System.out.println( (a<b) && (a==b) );     // false && false = false <<if any one is false then false>>
+        // System.out.println((a<b) || (a==b));       // true || false = false <<if any one is true then true>> (17-12-25 01:)
+        // System.out.println( !(a<b) );              // !(true) = false <<if true then false and if false then true>>
+
+        //Assignment Operators
+    //     int c = a; // c=5
+    //     c += 2; // c=c+2=7  //speed optimization
+    //     // System.out.println("The value of c is :" + c); //7    
+    // }
+
+}
