3bf00fc9521aef944b41d51db4f41c035e698a37
yug <yugchapherkar2@gmail.com>
2025-12-17 21:03:47 +0530

    day 6 vide code pushed--pattern printing

---
+++ javad6.java
@@ -0,0 +1,16 @@
+// Pattern Printing Day6 17-12-25
+
+import java.util.Scanner;
+public class javad6 {
+    public static void main (String args []) {
+        Scanner sc = new Scanner (System.in);
+        int line = sc.nextInt();
+        for (int i = 1; i <= line; i++) {
+            // for (int j = 1; j <= i; j++) {
+            //     System.out.print("* ");
+            // }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
