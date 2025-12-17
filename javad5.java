4cd186aa6701b8dce174eee0163e2d6e4c9fc7d6
yug <yugchapherkar2@gmail.com>
2025-12-17 15:42:46 +0530

    day 5 loop addded

---
+++ javad5.java
@@ -0,0 +1,33 @@
+// Loops Day 5
+
+import java.util.Scanner;
+public class javad5 {
+    public static void main (String[] args){
+        System.out.println("Enter any number:");
+        Scanner sc = new Scanner (System.in);
+        int count = sc.nextInt();
+        // int limit = 1;  //while loop (initialization before loop) 
+        // while (limit <= count) {
+        //     System.out.println(limit);
+        //     limit++;
+
+            //for loop for (initialization; condition; increment/decrement)
+            // for (int lines = 1; lines<= count; lines++)
+            //     System.out.println("****");
+
+            // while (lines<=4 ) {
+            //     System.out.println("****");
+            //     lines++;
+            // }
+
+            //do while loop do { //do something
+            //     // } while (condition);
+
+            do {
+                System.out.println("hello");
+                count++;
+            } while (count < 5);
+            }
+        }
+
+    
