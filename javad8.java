0ffe5bb282cec129085ba5737a625ebca951bd76
yug <yugchapherkar2@gmail.com>
2025-12-26 23:34:10 +0530

    day 8 half pattern

---
+++ javad8.java
@@ -0,0 +1,30 @@
+//Patterns printing with functions in Java
+import java.util.Scanner;
+public class javad8 {
+// hollow rectangle pattern  
+// public static void hollow_rectangle(int tot_rows, int tot_cols){
+//     //outer loop (i represents lines row
+//     for (int i=1; i<=tot_rows; i++){
+//        //inner loop (j represents columns)
+//         for (int j=1; j<=tot_cols; j++){
+//            // cell - (i,j)
+//             if (i==1 || i==tot_rows || j==1 || j==tot_cols){
+//                 System.out.print("*");
+//             }
+//             else{
+//                 System.out.print(" ");
+//             }
+//         }
+//         System.out.println();
+//     }
+// }
+
+
+
+
+
+
+    public static void main (String args[]){
+
+    }
+}
