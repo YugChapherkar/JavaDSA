d888972bef6404d33ef671395aa989a98c92c31c
yug <yugchapherkar2@gmail.com>
2025-12-27 23:20:41 +0530

    array inital commit

---
+++ javad9.java
@@ -0,0 +1,15 @@
+//Array Day 9
+//creating an array
+/* dataType[] arrayName = new dataType[size];   
+Creating an array
+int marks[]= new int [50]
+ */
+
+public class javad9 {
+    public static void main (String args[]){
+       
+        int marks[]= new int [50];
+        
+System.out.println("Welcome to Day 9 of Java");
+    }
+}
